a9ab40f7e8b2e6da3f5b4f297a5aba6bd4debe8d
paramedicjack <paramed1cj4ck@gmail.com>
2022-10-23 16:24:52 -0500

    Updated for enums - Upgraded and simplifier GUI - Added enum tab and enum functionality

--- README.md
+++ README.md
@@ -2,28 +2,76 @@
 
 shark-dgen is a Qt GUI program that allows you to create Lua dissectors in wireshark with no experience in Lua.
 
-![alt text](https://github.com/paramedicjack/shark-dgen/blob/main/photos/v0.1/filled.PNG?raw=true)
+![alt text](https://github.com/paramedicjack/shark-dgen/blob/main/photos/v0.2/filled.PNG?raw=true)
 
-![alt text](https://github.com/paramedicjack/shark-dgen/blob/main/photos/v0.1/output.PNG?raw=true)
+![alt text](https://github.com/paramedicjack/shark-dgen/blob/main/photos/v0.2/output.PNG?raw=true)
+
+## How to use
+
+### Setup
+
+1. Grab the the [latest release](https://github.com/paramedicjack/shark-dgen/releases)
+2. Extract the zip and run shark-dgen.exe
+
+### Add fields
+
+1. Click the fields tab
+2. Type a field name
+3. Select options - endianness, type, encoding
+4. Press ADD
+5. Verify that the field appears in the table and the first four columns in the row match
+
+You can select a row and press DELETE ROW to delete the row.
+
+It is not recommended to edit cells directly as this could cause faults or break your dissector.
+
+### Add enums
+
+Enums have only been tested on uints  and with = so far.
+
+1. Click the enums tab
+2. Select a row in the table
+3. Enter a key (name) and value (number) in the appropriate boxes
+4. Select an operation in the dropdown
+5. Press ADD
+6. Ensure that the enum has been added to the dropdown
+
+You can delete an enum value by selecting it in the dropdown and pressing DELETE.
+
+### Add info and ports
+
+1. Go to Info tab
+2. Enter a name in the name section
+3. Enter a 2-3 letter acronym in the acronym box
+4. Go to ports tab
+5. Type a port and press ADD
+6. Ensure that the ports you want are in the dropdown
+
+You can delete ports by selecting them in the dropdown and pressing the DELETE button.
+
+### Generate 
+
+1. Go to the Create tab
+2. Press Create
+3. Verify that the dissector looks how it should in the Dissector tab
+4. Press Save As... to export the dissector. Place it in the wireshark Lua plugins directory.
 
 ## Features
 
-1. Many common types such as bools, uints, ints, floats, doubles
-2. Little Endian and Big Endian order
-3. Base encoding types DEC, HEX, OCT, and NONE
-4. Custom acronym and naming conventions
-5. Single port binding
-6. File saving
-
-## Planned features
-
-1. More types
-2. More base encoding types
-3. Tighter naming and field control
-4. Text appending
-5. Reading CSV
-6. Subtrees
-7. Enumeration values
+| Feature  | Status | Version |
+| ------------- | ------------- | ------------- |
+| UDP dissectors | implemented | v0.1 |
+| uints, floats, doubles, chars, bools | implemented | v0.1 |
+| little and big endian | implemented | v0.1 |
+| DEC, HEX, OCT, and NONE encoding | implemented  | v0.1 |
+| save dissector to file | implemented | v0.1 |
+| multiple port binding | implemented | v0.2 |
+| enum values | implemented | v0.2 |
+| TCP, etc | not implemented | - |
+| more types | not implemented | - |
+| import and export from csv | not implemented | - |
+| text appending | not implemented | - |
+| nested subtrees | not implemented | - |
 
 ## Development and release
 
--- shark-dgen/mainwindow.cpp
+++ shark-dgen/mainwindow.cpp
@@ -40,7 +40,7 @@ void MainWindow::on_addButton_clicked()
                 ui->fieldTable->setItem(row, COLUMN_NAMES::INDEX, new QTableWidgetItem(QString::number(0)));
             }
             else {
-                int newIndex = ui->fieldTable->item(row-1, COLUMN_NAMES::INDEX)->text().toInt() + ui->fieldTable->item(row-1, 4)->text().toInt();
+                int newIndex = ui->fieldTable->item(row-1, COLUMN_NAMES::INDEX)->text().toInt() + ui->fieldTable->item(row-1, COLUMN_NAMES::LENGTH)->text().toInt();
                 ui->fieldTable->setItem(row, COLUMN_NAMES::INDEX, new QTableWidgetItem(QString::number(newIndex)));
 
             }
@@ -60,9 +60,33 @@ void MainWindow::on_deleteButton_clicked()
 }
 
 void MainWindow::errorCondition(QString error) {
-    ui->dissectorEdit->clear();
-    ui->dissectorEdit->insertPlainText(error);
+    ui->logEdit->clear();
+    ui->logEdit->setText(error);
 }
+
+
+QString MainWindow::getOperator(QString str) {
+    if(str.contains("=")) {
+        return "=";
+    }
+    if(str.contains(">")) {
+        return ">";
+    }
+    if(str.contains(">=")) {
+        return ">=";
+    }
+    if(str.contains("<")) {
+        return "<";
+    }
+    if(str.contains("<=")) {
+        return "<=";
+    }
+    if(str.contains("~=")) {
+        return "~=";
+    }
+    return "=";
+}
+
 
 
 void MainWindow::on_generateButton_clicked()
@@ -83,8 +107,25 @@ void MainWindow::on_generateButton_clicked()
     }
 
     if(ui->fieldTable->rowCount() > 0) {
+
+
         ui->dissectorEdit->insertPlainText(header);
+
+
+        // ENUM FUNCTION SECTION
+        for (const auto &enums : enumMap) {
+            ui->dissectorEdit->insertPlainText(enumFunctionHeader.arg(enums.first));
+            for (const auto& qstr : enums.second) {
+                QString splitOp = getOperator(qstr);
+                ui->dissectorEdit->insertPlainText(checkEnumValue.arg(qstr.split(splitOp)[1], qstr.split(splitOp)[0]));
+            }
+            ui->dissectorEdit->insertPlainText(enumNotFoundReturn);
+            ui->dissectorEdit->insertPlainText(functionEnd);
+            //ui->dissectorEdit->insertPlainText(checkEnumValue.arg(enums.split(" = ")[0], enums.split(" = ")[1]));
+        }
+
         ui->dissectorEdit->insertPlainText(protocol.arg(ui->nameEdit->toPlainText(), ui->acronymEdit->toPlainText()));
+
 
         for (int i = 0; i < ui->fieldTable->rowCount(); i++) {
             ui->dissectorEdit->insertPlainText(protoField.arg(ui->fieldTable->item(i, COLUMN_NAMES::ITEM)->text(), ui->fieldTable->item(i, COLUMN_NAMES::TYPE)->text().toLower(), ui->nameEdit->toPlainText(), ui->fieldTable->item(i, COLUMN_NAMES::BASE)->text()));
@@ -111,36 +152,167 @@ void MainWindow::on_generateButton_clicked()
 
 
         for (int i = 0; i < ui->fieldTable->rowCount(); i++) {
-            ui->dissectorEdit->insertPlainText(subtreeAdd.arg(ui->fieldTable->item(i, COLUMN_NAMES::ITEM)->text(), ui->fieldTable->item(i, COLUMN_NAMES::INDEX)->text(), ui->fieldTable->item(i, COLUMN_NAMES::LENGTH)->text()));
+            bool isAnEnum = false;
+            for (const auto &enums : enumMap) {
+
+                if (enums.first == ui->fieldTable->item(i, COLUMN_NAMES::ITEM)->text()) {
+
+                    auto it = protoTranslation.find(ui->fieldTable->item(i, COLUMN_NAMES::TYPE)->text());
+                    if(it != protoTranslation.end()) {
+                        ui->dissectorEdit->insertPlainText(subtreeAddAppend.arg(ui->fieldTable->item(i, COLUMN_NAMES::ITEM)->text(),
+                            ui->fieldTable->item(i, COLUMN_NAMES::INDEX)->text(), ui->fieldTable->item(i, COLUMN_NAMES::LENGTH)->text(),
+                               it->second));
+                        isAnEnum = true;
+                    }
+
+                }
+            }
+            if(!isAnEnum) {
+                ui->dissectorEdit->insertPlainText(subtreeAdd.arg(ui->fieldTable->item(i, COLUMN_NAMES::ITEM)->text(), ui->fieldTable->item(i, COLUMN_NAMES::INDEX)->text(), ui->fieldTable->item(i, COLUMN_NAMES::LENGTH)->text()));
+            }
+
         }
 
         ui->dissectorEdit->insertPlainText(functionEnd);
-        ui->dissectorEdit->insertPlainText(portBind.arg(ui->portEdit->toPlainText(), ui->nameEdit->toPlainText()));
+        //ui->dissectorEdit->insertPlainText(portBind.arg(ui->portEdit->toPlainText(), ui->nameEdit->toPlainText()));
+
+        for ( const auto& qstr : portList) {
+            ui->dissectorEdit->insertPlainText(portBind.arg(qstr, ui->nameEdit->toPlainText()));
+        }
+
+        if(!ui->saveAsButton->isEnabled()) ui->saveAsButton->setEnabled(true);
     }
 }
 
 
 void MainWindow::on_saveAsButton_clicked()
 {
-    QString fileName = QFileDialog::getSaveFileName(this,
-            tr("Save Dissector"), "",
-            tr("dissector (*.lua);;All Files (*)"));
-    if (fileName.isEmpty()) {
-        return;
-    }
+    if(!ui->dissectorEdit->document()->isEmpty()) {
+        QString fileName = QFileDialog::getSaveFileName(this,
+                tr("Save Dissector"), "",
+                tr("dissector (*.lua);;All Files (*)"));
+        if (fileName.isEmpty()) {
+            return;
+        }
 
-     else {
-         QFile file(fileName);
-         if (!file.open(QIODevice::WriteOnly)) {
-             QMessageBox::information(this, tr("Unable to open file"),
-                 file.errorString());
-             return;
-         }
+         else {
+             QFile file(fileName);
+             if (!file.open(QIODevice::WriteOnly)) {
+                 QMessageBox::information(this, tr("Unable to open file"),
+                     file.errorString());
+                 return;
+             }
 
-         QTextStream out(&file);
-         out << ui->dissectorEdit->toPlainText();
+             QTextStream out(&file);
+             out << ui->dissectorEdit->toPlainText();
 
+        }
     }
     return;
+}
+
+
+void MainWindow::on_fieldTable_itemSelectionChanged()
+{
+    QItemSelectionModel *select = ui->fieldTable->selectionModel();
+    if(select->hasSelection()) {
+        if(!ui->enumStringLine->isEnabled()) ui->enumStringLine->setEnabled(true);
+        if(!ui->enumValueLine->isEnabled()) ui->enumValueLine->setEnabled(true);
+        if(!ui->enumOperatorBox->isEnabled()) ui->enumOperatorBox->setEnabled(true);
+        if(!ui->enumAddButton->isEnabled()) ui->enumAddButton->setEnabled(true);
+
+        ui->enumBox->clear();
+        int row = ui->fieldTable->currentRow();
+        auto it = enumMap.find(ui->fieldTable->item(row, COLUMN_NAMES::ITEM)->text());
+        if (it == enumMap.end()) {
+            QStringList newQSL;
+            enumMap.try_emplace(ui->fieldTable->item(row, COLUMN_NAMES::ITEM)->text(), newQSL);
+        }
+        else {
+            ui->enumBox->addItems(it->second);
+        }
+    }
+
+    return;
+}
+
+
+void MainWindow::on_enumAddButton_clicked()
+{
+    QItemSelectionModel *select = ui->fieldTable->selectionModel();
+    if(select->hasSelection()) {
+        int row = ui->fieldTable->currentRow();
+        auto it = enumMap.find(ui->fieldTable->item(row, COLUMN_NAMES::ITEM)->text());
+        if (it != enumMap.end()) {
+            QString enumStr = ui->enumStringLine->toPlainText() + ui->enumOperatorBox->currentText() + ui->enumValueLine->toPlainText();
+            bool duplicate = false;
+            for (const auto& qstr : it->second) {
+                if(enumStr.split(ui->enumOperatorBox->currentText())[0] == qstr.split(ui->enumOperatorBox->currentText())[0]) {
+                    duplicate = true;
+                }
+            }
+            if (!duplicate) {
+                it->second << enumStr;
+                ui->enumBox->clear();
+                ui->enumBox->addItems(it->second);
+            }
+        }
+        if(ui->enumBox->count() > 0) {
+            ui->enumBox->setEnabled(true);
+            ui->enumDeleteButton->setEnabled(true);
+        }
+    }
+}
+
+
+void MainWindow::on_enumDeleteButton_clicked()
+{
+    QItemSelectionModel *select = ui->fieldTable->selectionModel();
+
+    if (select->hasSelection()) {
+        int row = ui->fieldTable->currentRow();
+        auto it = enumMap.find(ui->fieldTable->item(row, COLUMN_NAMES::ITEM)->text());
+        if (it != enumMap.end()) {
+            it->second.removeAll(ui->enumBox->currentText());
+            ui->enumBox->clear();
+            ui->enumBox->addItems(it->second);
+            if (ui->enumBox->count() == 0) {
+                ui->enumBox->setDisabled(true);
+                ui->enumDeleteButton->setDisabled(true);
+            }
+        }
+    }
+}
+
+
+void MainWindow::on_portAddButton_clicked()
+{
+    if(!ui->portEdit->toPlainText().isEmpty()) {
+        if(!portList.contains(ui->portEdit->toPlainText())) {
+            portList.append(ui->portEdit->toPlainText());
+            ui->portComboBox->clear();
+            ui->portComboBox->addItems(portList);
+        }
+        if(ui->portComboBox->count() > 0) {
+            ui->portComboBox->setEnabled(true);
+            ui->portDeleteButton->setEnabled(true);
+        }
+    }
+}
+
+
+void MainWindow::on_portDeleteButton_clicked()
+{
+    if(ui->portComboBox->count() > 0) {
+        portList.removeAll(ui->portComboBox->currentText());
+        ui->portComboBox->clear();
+        if(portList.count() > 0) {
+            ui->portComboBox->addItems(portList);
+        }
+        else {
+            ui->portComboBox->setEnabled(false);
+            ui->portDeleteButton->setEnabled(false);
+        }
+    }
 }
 
